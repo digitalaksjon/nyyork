1b09512430ba0218597e4ff7fabff803dfe393a3
Digital Aksjon <post@digitalaksjon.no>
2020-11-27 18:02:49 +0100

    set the title of posts to 2em

--- src/components/templates/styles/PostWrapper.js
+++ src/components/templates/styles/PostWrapper.js
@@ -79,7 +79,7 @@ background-color: ${nyYorkColors.gray};
                      font-size: 4rem;
                      text-transform: uppercase;
                      @media screen and (max-width: 415px) {
-                         font-size: 1.5rem;
+                         font-size: 2rem;
                      }
      
                  }
